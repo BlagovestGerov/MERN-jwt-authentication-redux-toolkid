b6f5c0c9f1a76782cd605d18a07d6df64a0f6f0e
blaje <blaje@abv.bg>
2023-05-19 13:43:15 +0300

    create registerUser, logoutUser, getUserProfile, updateUserProfile routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,10 +5,52 @@ import asyncHandler from 'express-async-handler';
 // @access Public
 
 const authUser = asyncHandler(async (req, res) => {
-    res.status(401);
-    throw new Error('Something went wrong');
-
     res.status(200).json({ message: 'Auth User' })
 });
 
-export { authUser };
+
+// @desc Regiter a new user
+// route POST /api/user
+// @access Public
+
+const registerUser = asyncHandler(async (req, res) => {
+
+    res.status(200).json({ message: 'Register User' })
+});
+
+
+// @desc Logout user
+// route POST /api/user/logout
+// @access Public
+
+const logoutUser = asyncHandler(async (req, res) => {
+
+    res.status(200).json({ message: 'Logout User' })
+});
+
+// @desc GET user profile
+// route GET /api/user/profile
+// @access Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+
+    res.status(200).json({ message: 'User Profile' })
+});
+
+
+// @desc UPDATE user profile
+// route PUT /api/user/profile
+// @access Private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+
+    res.status(200).json({ message: 'Update User Profile' })
+});
+
+export {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
+};

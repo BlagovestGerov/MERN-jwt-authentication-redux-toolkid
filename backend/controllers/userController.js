398cc94318ead32ca9e20881313f2fa6e38f374b
blaje <blaje@abv.bg>
2023-05-19 10:49:50 +0300

    custom error handler using express-async-handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,14 @@
+import asyncHandler from 'express-async-handler';
+
 // @desc   Auth user/set token
 // route POST /api/user/auth
 // @access Public
 
-const authUser = async (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
+    res.status(401);
+    throw new Error('Something went wrong');
+
     res.status(200).json({ message: 'Auth User' })
-}
+});
 
 export { authUser };

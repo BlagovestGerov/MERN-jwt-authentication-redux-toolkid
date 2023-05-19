ded5b4ab9e05d49313318985f2c90a64801bc7b2
blaje <blaje@abv.bg>
2023-05-19 10:28:44 +0300

    create authUser router

---
+++ backend/controllers/userController.js
@@ -0,0 +1,9 @@
+// @desc   Auth user/set token
+// route POST /api/user/auth
+// @access Public
+
+const authUser = async (req, res) => {
+    res.status(200).json({ message: 'Auth User' })
+}
+
+export { authUser };

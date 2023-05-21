2e2c85dfd7ac4b6a4977f8e075e9575956e50bc4
blaje <blaje@abv.bg>
2023-05-21 18:03:45 +0300

    auth user endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,27 @@
 import asyncHandler from 'express-async-handler';
 import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js';
+
 // @desc   Auth user/set token
 // route POST /api/user/auth
 // @access Public
 
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Auth User' })
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        generateToken(res, user._id)
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(401);
+        throw new Error('Invlid email or password');
+    }
 });
 
 

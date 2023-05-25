f3fc146d67dcba1ffb04af0217525985bb71c653
blaje <blaje@abv.bg>
2023-05-25 15:20:21 +0300

    prepare for deployment in production

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from "./config/db.js";
 const port = process.env.PORT || 5000;
 import userRoutes from './routes/userRoutes.js';
+import path from 'path';
 
 connectDB();
 
@@ -17,6 +18,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
+} else {
+    app.get('/', (req, res) => res.send('Server is ready!'));
+}
+
 
 app.get('/', (req, res) => res.send('Server is ready!'));
 
